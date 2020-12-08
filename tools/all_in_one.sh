62e75b47d86edd539c732a5858de868964768599
jixingxing <542255405@qq.com>
2020-12-08 19:32:27 +0800

    Add ubuntu suppored for deploymen
    Co-Authored-By: caoyingjun <cao.yingjunz@gmail.com>
    
    Change-Id: I18c5b7c0eeb349f99083c522aee1d30c086ba4ec

--- tools/all_in_one.sh
+++ tools/all_in_one.sh
@@ -1,15 +1,49 @@
 #!/usr/bin/env bash
 
-curl http://mirrors.aliyun.com/repo/Centos-7.repo -o /etc/yum.repos.d/CentOS-Base.repo
-yum install -y epel-release
-yum install -y git python-pip ansible
+linux_os(){
+    a=`uname -a`
+    if [[ $a =~ Ubuntu ]];then
+            ubuntu_shell ;
+    else    centos_shell ;
+    fi
+    }
+
+centos_shell(){
+    curl http://mirrors.aliyun.com/repo/Centos-7.repo -o /etc/yum.repos.d/CentOS-Base.repo
+    yum install -y epel-release
+    yum install -y git python-pip ansible
+}
+
+ubuntu_shell(){
+    systemctl disable systemd-resolved
+    systemctl stop systemd-resolved
+    cp /etc/apt/sources.list /etc/apt/sources.list.bak
+    echo "nameserver 114.114.114.114" >>/etc/resolv.conf
+
+    cat << EOF > /etc/apt/sources.list
+    deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+    deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+    deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+    deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+    deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+    deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+    deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+    deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+    deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+    deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
+    sudo apt -y update
+    apt install -y git python-pip ansible
+}
 
 mkdir -p ~/.pip
 cat << EOF > ~/.pip/pip.conf
 [global]
-trusted-host =  mirrors.aliyun.com
+trusted-host = mirrors.aliyun.com
 index-url = http://mirrors.aliyun.com/pypi/simple/
 EOF
+
+linux_os
 
 if [[ ! -d /tmp/kubez-ansible ]]; then
     git clone https://github.com/yingjuncao/kubez-ansible /tmp/kubez-ansible
