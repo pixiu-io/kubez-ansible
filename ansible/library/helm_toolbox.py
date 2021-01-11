e01f0f4c94fda213c71bb0130b6843a63f871eef
jixingxing <5422554051@qq.com>
2021-01-11 14:36:20 +0800

    Install the Harbor applications of type NodePort through Helm3
    Change-Id: I993e7377d0c9584b116353eb0a55877d18998f3d

--- ansible/library/helm_toolbox.py
+++ ansible/library/helm_toolbox.py
@@ -89,6 +89,10 @@ class Helm3Worker(object):
             self.run_cmd(' '.join(cmd))
             self.changed = True
 
+    def uninstall(self):
+        cmd = ['helm', 'uninstall', self.name, '-n', self.namespace]
+        self.run_cmd(cmd)
+
     @property
     def is_installed(self):
         charts = self.run_cmd(
@@ -103,6 +107,7 @@ def main():
     specs = dict(
         name=dict(required=True, type='str'),
         namespace=dict(required=False, type='str', default='default'),
+        action=dict(required=True, type='str'),
         chart=dict(required=True, type='str'),
         chart_extra_vars=dict(type='json')
     )
@@ -112,7 +117,7 @@ def main():
     hw = None
     try:
         hw = Helm3Worker(params)
-        getattr(hw, 'install')()
+        getattr(hw, params.get('action'))()
         module.exit_json(changed=hw.changed, result=hw.result)
     except Exception:
         module.fail_json(changed=True, msg=repr(traceback.format_exc()),
