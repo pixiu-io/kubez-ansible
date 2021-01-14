e02a0a9241f395a8715947f41fcd20c33d60f0c7
caoyingjun <cao.yingjunz@gmail.com>
2021-01-15 01:07:57 +0800

    Optimise the helm install
    Change-Id: I3d8c43f0b0303ca452b8aaa43604d060ca2190ae

--- ansible/roles/kubernetes/files/setup_helm.sh
+++ ansible/roles/kubernetes/files/setup_helm.sh
@@ -3,9 +3,27 @@
 # NTOE(caoyingjun): Ths script complete the helm3 installed for temp,
 # but it will be optimise laster
 
-IMAGE=$1
+function _is_installed {
+    type $1 >/dev/null 2>&1;
+    [[ "$?" == "$1" ]]
+}
 
-docker run -d --name helm_toolbox $IMAGE
-docker cp helm_toolbox:/usr/bin/helm /usr/bin/helm
-chmod +x /usr/bin/helm
-docker rm helm_toolbox -f
+function is_docker_installed {
+    _is_installed "docker"
+}
+
+function is_helm_installed {
+    _is_installed "helm"
+}
+
+function helm_installed {
+    if !is_helm_installed {
+        docker run -d --name helm_toolbox jacky06/helm-toolbox:v3.0.3 && \
+        docker cp helm_toolbox:/usr/bin/helm /usr/bin/helm && \
+        chmod +x /usr/bin/helm && \
+        docker rm helm_toolbox -f
+    }
+}
+
+is_docker_installed
+helm_installed
