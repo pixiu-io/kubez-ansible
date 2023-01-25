b50f73dcc7bdb419435f17293fef1d32a223da3d
caoyingjunz <cao.yingjunz@gmail.com>
2023-01-25 18:56:18 +0800

    add
    Change-Id: I292f2325c7b567758574826f24ed8034442e9510

--- ansible/roles/kubernetes/files/setup_helm.sh
+++ ansible/roles/kubernetes/files/setup_helm.sh
@@ -1,48 +1,29 @@
 #!/usr/bin/env bash
 
-# NTOE(caoyingjun): Ths script complete the helm3 installation for centos.
+# NTOE(caoyingjun): Ths script complete the helm installation.
 
-IMAGE=$1
-
-function is_docker_installed {
-    type docker >/dev/null 2>&1;
-    [[ "$?" == "0" ]]
-}
+NAMESPACE=$1
 
 function is_helm_installed {
     type helm >/dev/null 2>&1;
     [[ "$?" == "0" ]]
 }
 
-function toolbox_image_exists {
-    REPO=$(echo ${IMAGE} | awk -F ':' '{print $1}')
-    TAG=$(echo ${IMAGE} | awk -F ':' '{print $2}')
-    docker images | grep ${REPO} | grep ${TAG}
-    [[ "$?" == "0" ]]
-}
-
-function helm_installed {
+function install_helm {
     if ! is_helm_installed; then
-        if is_docker_installed; then
-            # Pull the toolbox image when not exists
-            if ! toolbox_image_exists; then
-                docker pull ${IMAGE}
-            fi
+        # Wait for helm_toolbox up
 
-            # Install the command by copy from container
-            docker run -d --name helm_toolbox ${IMAGE} && \
-            docker cp helm_toolbox:/usr/bin/helm /usr/bin/helm && \
-            chmod +x /usr/bin/helm && \
-            docker rm helm_toolbox -f
-            echo "Install helm done" 1>&2
-        else
-            # For now, the docker is necessary when helm installed
-            echo "Docker is not installed, but necessary" 1>&2
+        # install helm
+        kubectl cp helm-toolbox-0:usr/bin/helm /usr/bin/helm -n ${NAMESPACE}
+        if [ $? -ne 0 ]; then
+            echo "failed to copy helm from helm-toolbox" 1>&2
             exit 1
         fi
+
+        chmod +x /usr/bin/helm
     else
         echo "Helm has been installed" 1>&2
     fi
 }
 
-helm_installed
+install_helm
