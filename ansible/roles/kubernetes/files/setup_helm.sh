16a23a77177b3f7a3050c49ebe775258f3b59380
pixiuio <121279164+pixiuio@users.noreply.github.com>
2023-01-26 14:06:36 +0800

    wait for helm-toolbox-0 up

--- ansible/roles/kubernetes/files/setup_helm.sh
+++ ansible/roles/kubernetes/files/setup_helm.sh
@@ -12,7 +12,20 @@ function is_helm_installed {
 function install_helm {
     if ! is_helm_installed; then
         # Wait for helm_toolbox up
-        kubectl get pod helm-toolbox-0 -n ${NAMESPACE} | grep helm-toolbox-0 | awk '{print $2}'
+        retries=0
+        while true; do
+            if [ $retries -ge 30 ]; then
+                echo "failed to wait helm-toolbox up" 1>&2
+                exit 1
+            fi
+            ready=$(kubectl get pod helm-toolbox-0 -n ${NAMESPACE} | grep helm-toolbox-0 | awk '{print $2}')
+            if [[ "$ready" == "1/1" ]]; then
+                break
+            fi
+
+            sleep 1
+            retries+=1
+        done
 
         # install helm
         kubectl cp helm-toolbox-0:usr/bin/helm /usr/bin/helm -n ${NAMESPACE}
@@ -20,7 +33,6 @@ function install_helm {
             echo "failed to copy helm from helm-toolbox" 1>&2
             exit 1
         fi
-
         chmod +x /usr/bin/helm
     else
         echo "Helm has been installed" 1>&2
