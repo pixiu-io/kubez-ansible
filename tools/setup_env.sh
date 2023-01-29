9bf838bece70ffaeb30d37d6baad8c44308fc0b3
Swm-rose <64686398+Swm-rose@users.noreply.github.com>
2023-01-29 17:36:33 +0800

    Update setup_env.sh

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -129,7 +129,7 @@ function install_kubez_ansible {
 
     install_ansible
 
-    pip install /tmp/kubez-ansible/requirements.txt
+    pip install -r /tmp/kubez-ansible/requirements.txt
     pip install /tmp/kubez-ansible/
 }
 
