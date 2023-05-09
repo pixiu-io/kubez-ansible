5bf2278c67f47a6bce6699c9aca205aa7d55c619
caoyingjunz <cao.yingjunz@gmail.com>
2023-05-09 23:28:07 +0800

    add

--- ansible/library/kube_toolbox.py
+++ ansible/library/kube_toolbox.py
@@ -117,13 +117,6 @@ class KubeWorker(object):
         return False
 
     @property
-    def is_get_sandbox(self):
-        if (self.module_name == 'kubeadm'
-           and self.module_args.startswith('sandbox')): # noqa
-            return True
-        return False
-
-    @property
     def is_kubectl(self):
         if self.module_name == 'kubectl':
             return True
@@ -275,6 +268,9 @@ class KubeWorker(object):
             # False.
             if not self.module_args.startswith('apply'):
                 self.changed = True
+
+            # 目前仅处理 images 获取
+            # if self.module_args.startswith('config'):
             self.result['kube_result'] = kube_result
 
     def get(self):
