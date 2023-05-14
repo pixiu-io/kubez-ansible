2d1175eb8bfe78e7ec36bed2cf623aa12de55c9e
caoyingjunz <cao.yingjunz@gmail.com>
2023-05-14 13:22:35 +0800

    Update kube_runtime.py

--- ansible/library/kube_runtime.py
+++ ansible/library/kube_runtime.py
@@ -67,6 +67,7 @@ class RuntimeBase(object):
                                 stderr=subprocess.PIPE,
                                 shell=True)
         stdout, stderr = proc.communicate()
+        stdout, stderr = stdout.decode(), stderr.decode()
         retcode = proc.poll()
         if retcode != 0:
             output = 'stdout: "%s", stderr: "%s"' % (stdout, stderr)
