4d8c6cf07db2018b9b3090516d3f41cc30913dfe
caoyingjunz <cao.yingjunz@gmail.com>
2024-03-02 14:34:15 +0800

    add

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,6 @@
 set -o errexit
 set -o xtrace
 
-if [[ ! -d "/etc/kubez" ]]; then
-    mkdir -p /etc/kubez
-fi
 cp /configs/globals.yml /etc/kubez/globals.yml
 cp /configs/multinode /etc/kubez/multinode
 cp /configs/hosts /etc/hosts
