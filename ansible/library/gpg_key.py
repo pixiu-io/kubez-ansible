c6217ce46136f004033d98091cc919bedc3dea00
caoyingjunz <cao.yingjunz@gmail.com>
2024-08-04 10:52:57 +0800

    add

--- ansible/library/gpg_key.py
+++ ansible/library/gpg_key.py
@@ -1,4 +1,4 @@
-# Copyright 2024 YourName
+# Copyright 2024 Pixiu
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,11 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import subprocess
 
+import subprocess
 import requests
 import os
 import traceback
+
 from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = '''
@@ -44,44 +45,53 @@ class GPGKey(object):
         self.params = params
         self.url = self.params.get('url')
         self.output_path = self.params.get('output_path')
+
         self.temp_path = '/tmp/temp_gpg_key'
+
         self.changed = False
         self.result = {}
+
+    def run_cmd(self, cmd):
+        proc = subprocess.Popen(cmd,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                shell=True)
+        stdout, stderr = proc.communicate()
+        stdout, stderr = stdout.decode(), stderr.decode()
+        retcode = proc.poll()
+        if retcode != 0:
+            output = 'cmd: "%s", code: "%s" stdout: "%s", stderr: "%s"' % (cmd, retcode, stdout, stderr)
+            raise Exception(output)
+        return stdout.rstrip()
 
     def download_file(self):
         response = requests.get(self.url, stream=True)
         if response.status_code == 200:
-            with open(self.temp_path, 'wb') as file:
-                file.write(response.content)
-            return True
+            with open(self.temp_path, 'wb') as f:
+                f.write(response.content)
         else:
-            return False
+            # TODO: raise errors
+            raise Exception("failed to download gpg key")
 
     def convert_key(self):
-        result = subprocess.run(['gpg', '--yes', '--dearmor', '-o', self.output_path, self.temp_path],
-                                capture_output=True, text=True)
-        if result.returncode == 0:
-            return True
-        else:
-            return False
+        self.run_cmd(' '.join(['gpg', '--yes', '--dearmor', '-o', self.output_path, self.temp_path]))
+        self.changed = True
 
-    def create_dir(self):
+    def ensure_gpg_dir_exists(self):
         os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
+
+    def install_gpg(self):
+        self.ensure_gpg_dir_exists()
+
+        # download gpg file
+        self.download_file()
+        self.convert_key()
 
     def process(self):
         if os.path.exists(self.output_path):
-            self.result['msg'] = 'GPG key already exists'
             return
-        self.create_dir()
-        if self.download_file():
-            self.changed = True
-            if self.convert_key():
-                self.result['msg'] = 'GPG key downloaded and converted successfully'
-            else:
-                self.result['msg'] = 'Failed to convert the GPG key'
-            os.remove(self.temp_path)
-        else:
-            self.result['msg'] = 'Failed to download the GPG key'
+
+        self.install_gpg()
 
 
 def main():
