f63e78a6280bc60b770e0e7a734c6b46ce740e1a
puzhihao <127116692+puzhihao@users.noreply.github.com>
2026-08-10 11:29:44 +0800

    修复镜像部署时自定义源不存在问题 (#567)

--- docker/start.sh
+++ docker/start.sh
@@ -10,4 +10,8 @@ cp /configs/globals.yml /etc/kubez/globals.yml
 cp /configs/multinode /etc/kubez/multinode
 cp /configs/hosts /etc/hosts
 
+if [[ -f "/configs/pixiu" ]]; then
+      cp /configs/pixiu /etc/kubez/pixiu
+fi
+
 kubez-ansible -i /etc/kubez/multinode ${COMMAND}
